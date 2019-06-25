f2b4fcf664e9aa7edb422492e68aa0f7c0f808e3
Yusuf Yogi Antoro Makfud <52063644+yusufyogia@users.noreply.github.com>
2019-06-25 07:52:20 +0700

    Add files via upload

---
+++ input.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pengolahanfrom</title>
+</head>
+<body>
+	<from ACTION="proses02.php" method="POST" name="input">
+		nama anda : <input type="text" name="nama"><br>
+		<input type="submit" name="input" value="input">
+	</from>
+</body>
+</html>
---
+++ input021.php
@@ -0,0 +1,6 @@
+<?php
+if (isset($_POST['input'])) {
+	$nama = $_POST['nama'];
+	echo "nama anda : <b>$nama</b>";
+}
+?>
